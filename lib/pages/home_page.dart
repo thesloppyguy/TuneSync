862a77e0257ce5ef07caefabb0592061b501e221
thesloppyguy <sahilmandal@outlook.com>
2023-05-20 02:14:37 +0530

    v1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:alan_voice/alan_callback.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:myapp/model/radio.dart';
 import 'package:myapp/utils/ai_util.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:alan_voice/alan_voice.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,19 +20,89 @@ class _HomePageState extends State<HomePage> {
   late MyRadio _selectedRadio;
   // late Color _selectedColor;
   bool _isPlaying = false;
+  final sugg = [
+    "Play",
+    "Stop",
+    "Play rock music",
+    "Play 107 FM",
+    "Play next",
+    "Play 104 FM",
+    "Pause",
+    "Play previous",
+    "Play pop music"
+  ];
 
   final AudioPlayer _audioPlayer = AudioPlayer();
 
   fetchRadios() async {
     final radioJson = await rootBundle.loadString('assets/radio.json');
     radios = MyRadioList.fromJson(radioJson).radios;
+    _selectedRadio = radios[0];
     setState(() {});
+  }
+
+  setupAlan() {
+    AlanVoice.addButton(
+        "836411d4d7ed67ca500731ea282b903d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+        buttonAlign: AlanVoice.BUTTON_ALIGN_LEFT);
+
+    AlanVoice.callbacks.add((command) => _handleCommand(command.data));
+  }
+
+  _handleCommand(Map<String, dynamic> response) {
+    switch (response["command"]) {
+      case "play":
+        _playMusic(_selectedRadio.url);
+        break;
+      case "play_channel":
+        final id = response['id'];
+        _audioPlayer.pause();
+        MyRadio newRadio = radios.firstWhere((element) => element.id == id);
+        radios.remove(newRadio);
+        radios.insert(0, newRadio);
+        _playMusic(newRadio.url);
+        break;
+      case "stop":
+        _audioPlayer.stop();
+        break;
+      case "next":
+        final index = _selectedRadio.id;
+        MyRadio newRadio;
+        if (index + 1 > radios.length) {
+          newRadio = radios.firstWhere((element) => element.id == 1);
+          radios.remove(newRadio);
+          radios.insert(0, newRadio);
+        } else {
+          newRadio = radios.firstWhere((element) => element.id == index + 1);
+          radios.remove(newRadio);
+          radios.insert(0, newRadio);
+          _playMusic(newRadio.url);
+        }
+        break;
+      case "prev":
+        final index = _selectedRadio.id;
+        MyRadio newRadio;
+        if (index - 1 <= 0) {
+          newRadio = radios.firstWhere((element) => element.id == 1);
+          radios.remove(newRadio);
+          radios.insert(0, newRadio);
+        } else {
+          newRadio = radios.firstWhere((element) => element.id == index - 1);
+          radios.remove(newRadio);
+          radios.insert(0, newRadio);
+          _playMusic(newRadio.url);
+        }
+        break;
+      default:
+        print("$response['command']");
+        break;
+    }
   }
 
   @override
   void initState() {
     super.initState();
-
+    setupAlan();
     fetchRadios();
 
     _audioPlayer.onPlayerStateChanged.listen((event) {
@@ -53,29 +125,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
+      drawer: Drawer(
+        child: Container(
+          color: AIColors.primaryColor2,
+          child: radios != null
+              ? [
+                  100.heightBox,
+                  "All Channels".text.xl.white.semiBold.make().px16(),
+                  20.heightBox,
+                  ListView(
+                    padding: Vx.m0,
+                    shrinkWrap: true,
+                    children: radios
+                        .map((e) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(e.icon),
+                              ),
+                              title: "${e.name} FM".text.white.make(),
+                              subtitle: e.tagline.text.white.make(),
+                            ))
+                        .toList(),
+                  ).expand()
+                ].vStack(crossAlignment: CrossAxisAlignment.start)
+              : const Offstage(),
+        ),
+      ),
       body: Stack(
         fit: StackFit.expand,
         children: [
           VxAnimatedBox()
               .size(context.screenWidth, context.screenHeight)
               .withGradient(LinearGradient(
-                  colors: [AIColors.primaryColor1, AIColors.primaryColor2],
+                  colors: [AIColors.primaryColor2, AIColors.primaryColor1],
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight))
               .make(),
-          AppBar(
-            title: "AI Radio".text.bold.white.make().shimmer(
-                primaryColor: Vx.purple300, secondaryColor: Colors.white),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            centerTitle: true,
-          ).h(100.0).p16(),
+          [
+            AppBar(
+              title: "AI Radio".text.bold.white.make().shimmer(
+                  primaryColor: Vx.purple300, secondaryColor: Colors.white),
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              centerTitle: true,
+            ).h(100.0).p16(),
+            "Start with hey Alan".text.italic.semiBold.white.make(),
+            10.heightBox,
+            VxSwiper.builder(
+                itemCount: sugg.length,
+                height: 50.0,
+                viewportFraction: 0.35,
+                autoPlay: true,
+                autoPlayAnimationDuration: 3.seconds,
+                autoPlayCurve: Curves.linear,
+                enableInfiniteScroll: true,
+                itemBuilder: (context, index) {
+                  final s = sugg[index];
+                  return Chip(
+                    label: s.text.make(),
+                    backgroundColor: Vx.randomColor,
+                  );
+                })
+          ].vStack(),
+          30.heightBox,
           radios != null
               ? VxSwiper.builder(
                   itemCount: radios.length,
                   aspectRatio: 1.0,
                   enlargeCenterPage: true,
+                  onPageChanged: (index) {
+                    _selectedRadio = radios[index];
+                  },
                   itemBuilder: (context, index) {
                     final rad = radios[index];
                     return VxBox(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import package_info
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
